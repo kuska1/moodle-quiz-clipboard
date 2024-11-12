ef703ef6daca0e5d6a61232d3a8302a60d516e2d
Bogdan Kus <61652465+kuska1@users.noreply.github.com>
2024-11-12 19:52:47 +0200

    Changed numbers and letters

--- content.js
+++ content.js
@@ -1,38 +1,40 @@
 
 function extractQuestionAndAnswers() {
-  // Question
+  // Question label
   const questionElement = document.querySelector('.qtext p[dir="ltr"]');
   const questionText = questionElement ? questionElement.innerText : chrome.i18n.getMessage("question_not_found");
 
-  let fullText;
-
-  // First block
-  const firstQuestionBlock = questionElement.closest('.content'); // Замените '.content' на правильный родительский класс, если необходимо
+  // Question block
+  const firstQuestionBlock = questionElement.closest('.content');
   const selectElements = firstQuestionBlock ? firstQuestionBlock.querySelectorAll('.control select') : [];
   const labelElements = firstQuestionBlock ? firstQuestionBlock.querySelectorAll('.text p[dir="ltr"]') : [];
 
+  let fullText;
+
   if (selectElements.length > 0 && labelElements.length > 0) {
-      // Select found
-      const subQuestions = Array.from(labelElements).map((label, index) => `${index + 1}. ${label.innerText.trim()}`);
+      // Subquestions found (SelectElements)
+      const subQuestions = Array.from(labelElements).map((label, index) => `${String.fromCharCode(97 + index)}. ${label.innerText.trim()}`);
       
       const answerOptions = Array.from(selectElements[0].options)
           .slice(1) // Skip first element
-          .map((option, index) => `${String.fromCharCode(97 + index)}. ${option.text.trim()}`);
-      // Text format
+          .map((option, index) => `${index + 1}. ${option.text.trim()}`);
+      // Text formatting (Question\na.Answer\n1.SubAnswer)
       fullText = `${questionText}\n${subQuestions.join('\n')}\n\n${answerOptions.join('\n')}`;
   } else {
-      // Select not found
+      // Subquestions not found (SelectElements)
       const answers = Array.from(firstQuestionBlock.querySelectorAll('.answer p[dir="ltr"]'))
-          .map((answer, index) => `${index + 1}. ${answer.innerText.trim()}`);
-      // Text format
+          .map((answer, index) => `${String.fromCharCode(97 + index)}. ${answer.innerText.trim()}`);
+      // Text formatting (Question\na.Answer)
       fullText = `${questionText}\n${answers.join('\n')}`;
   }
-
+  // Just debug
+  console.debug("[MQC] "+chrome.i18n.getMessage("alert"));
   return fullText;
 }
 
 // Copyboard
 function copyTextToClipboard(event) {
+  // Prevent some unusual js scripts
   event.preventDefault();
   event.stopPropagation();
   const textToCopy = extractQuestionAndAnswers()
@@ -47,14 +49,17 @@ function copyTextToClipboard(event) {
 function addCopyButton() {
   const possearch = document.querySelector('.content');
   if (possearch) {
-    const copyButton = document.createElement("no");
+    // Successfully added button
+    const copyButton = document.createElement("MQC");
     copyButton.textContent = chrome.i18n.getMessage("button_text");
     copyButton.classList.add("copy-button");
     copyButton.addEventListener("click", copyTextToClipboard);
-    possearch.insertAdjacentElement("afterend", copyButton);
+    possearch.insertAdjacentElement("beforeend", copyButton);
   } else {
-    console.log("[MQC] "+chrome.i18n.getMessage("button_error"));
+    // Failed to add button
+    console.debug("[MQC] "+chrome.i18n.getMessage("button_error"));
   }
 }
 
+// Wait until page fully load
 window.addEventListener("load", addCopyButton);
